98a93c589bc8aafe1b917b07085badd04a873aa0
Gabrieltupi19072008 <gabrieltupi19072008@gmail.com>
2025-08-10 10:54:21 -0300

    Update q2.py

---
+++ q2.py
@@ -0,0 +1,9 @@
+# "id": 2,
+#    "titulo": "Encontrando o Maior Número",
+#    "descricao": "Escreva uma função que receba uma lista de números e retorne o maior número encontrado nela.
+
+
+def maior_numero(lista):
+    return max(lista)
+
+print(maior_numero( numeros = [987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9]))
